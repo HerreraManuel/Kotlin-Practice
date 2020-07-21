d7640bb86a989a199934e2401ae0235b661b31cc
HerreraManuel <mherrera.jm@gmail.com>
2020-07-21 16:12:37 -0700

    basic flow control

--- HelloKotlin/src/Main.kt
+++ HelloKotlin/src/Main.kt
@@ -24,4 +24,26 @@ fun main() {
     fish = null
     println(fish)
 
+    // type inference
+    var greeting: String? = "hi"
+    val number = 2
+    println(greeting)
+    println(number)
+
+    // Basic Flow Control
+    greeting = null
+    if(greeting != null){
+        println("Hi")
+    } else{
+        println("This has become a null")
+    }
+    greeting = "Hello"
+    when (greeting) {
+        null -> println("This is still a null")
+        else -> println(greeting)
+    }
+
+    greeting = null
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
+    println(greetingToPrint)
 }
