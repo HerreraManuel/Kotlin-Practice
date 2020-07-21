4b1c11702ef25569fd87a455c553af2544598026
HerreraManuel <mherrera.jm@gmail.com>
2020-07-21 15:58:14 -0700

    nullable variables

--- HelloKotlin/src/Main.kt
+++ HelloKotlin/src/Main.kt
@@ -1,3 +1,6 @@
+var firstName: String = "Mike"
+val lastName: String = "Smith"
+
 fun main() {
     println("Hello Kotlin")
 
@@ -10,5 +13,15 @@ fun main() {
     println(nickname)
     nickname = "Manuel"
     println(nickname)
+
+    // Using global variables
+    println(firstName)
+    println(lastName)
+
+    // nullable variable
+    var fish : String? = "goldfish"
+    println(fish)
+    fish = null
+    println(fish)
 
 }
