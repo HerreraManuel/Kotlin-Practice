7bc822cdef71cb3df855e296ab3298d6679561a8
HerreraManuel <mherrera.jm@gmail.com>
2020-07-21 15:52:48 -0700

    immutable and mutable variables

--- HelloKotlin/src/Main.kt
+++ HelloKotlin/src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
     println("Hello Kotlin")
+
+    // immutable variable
+    val name: String = "Manuel"
+    println(name)
+
+    // mutable variable
+    var nickname: String = "Kyle"
+    println(nickname)
+    nickname = "Manuel"
+    println(nickname)
+
 }
